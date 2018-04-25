b88ccdb5b61760c1bc2b01e0993ed1ea61b51fc7
ZETES\eduard.campo <eduard.campo@sf.zetes.com>
2018-04-25 11:02:27 +0200

    First commit

---
+++ FormApp/FormApp/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormApp
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public bool IsAdmin { get; set; }
+        //public Sex Sex { get; set; }
+    }
+
+    public enum Sex
+    {
+        Male = 0,
+        Female = 1
+    }
+}
---
+++ FormApp/FormApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormsGenerator;
+using Xamarin.Forms;
+
+namespace FormApp
+{
+	public partial class MainPage : ContentPage
+	{
+	    public Employee EmployeeModel { get; set; }
+		public MainPage()
+		{
+			InitializeComponent();
+		    EmployeeModel = new Employee();
+        }
+
+	    async void GenerateFormPage(object sender, EventArgs e)
+	    {
+	        var formGenerator = new FormGenerator();
+	        var formPage = formGenerator.GeneratePage<Employee>(EmployeeModel);
+	        await Navigation.PushAsync(formPage);
+	    }
+	}
+}
